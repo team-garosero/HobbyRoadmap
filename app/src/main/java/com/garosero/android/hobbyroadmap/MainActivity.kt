6632a44db236671a5668622aec2118671bc9ad5d
youngyin <57608725+youngyin@users.noreply.github.com>
2022-04-12 19:16:45 +0900

    tool bar & status bar
    change toolbar
    show Status bar
    rename bottom items

--- app/src/main/java/com/garosero/android/hobbyroadmap/MainActivity.kt
+++ app/src/main/java/com/garosero/android/hobbyroadmap/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.garosero.android.hobbyroadmap
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -12,9 +14,26 @@ class MainActivity : AppCompatActivity() {
         val binding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // toolbar
+        setSupportActionBar(binding.toolbar)
+
         // navigation
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host)
         val navController = navHostFragment!!.findNavController()
         binding.navView.setupWithNavController(navController)
+    }
+
+    // set toolbar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.items_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.item_setting -> {}
+            R.id.item_notice -> {}
+        }
+        return true
     }
 }
