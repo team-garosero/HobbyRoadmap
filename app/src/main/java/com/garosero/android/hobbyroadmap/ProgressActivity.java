2b947f842c9df171fde5ae28080b25497a7e1d3f
bobaejeon <bbj5608@gmail.com>
2022-07-13 15:18:40 +0900

    prrogressactivity troubleshot

--- app/src/main/java/com/garosero/android/hobbyroadmap/ProgressActivity.java
+++ app/src/main/java/com/garosero/android/hobbyroadmap/ProgressActivity.java
@@ -21,13 +21,17 @@ import com.garosero.android.hobbyroadmap.network.response.UserResponse;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 //6-7초 소요
 public class ProgressActivity extends AppCompatActivity {
-    final String API_KEY = "CLaq8zUYyGtiDmwvSX0dVOYdP10C7dBQzSXaV9j%2BW%2BErU5N0jzyAomUqqPnzBWCR8YggYr6%2FBEbCvksX2BQCsw%3D%3D";
+//    final String API_KEY = "CLaq8zUYyGtiDmwvSX0dVOYdP10C7dBQzSXaV9j%2BW%2BErU5N0jzyAomUqqPnzBWCR8YggYr6%2FBEbCvksX2BQCsw%3D%3D";
+    final String API_KEY = "8J6CPUUandk0YLso7B5y/z0I67lSDrVpiQhJcmagUHOlDHLTkuQA84FEMIt3Ii//T2DDh/4lOxwMFhPJzFA6Mg==";
     final String TAG = "ProgressActivity";
 
     @SuppressLint("Range")
@@ -54,12 +58,41 @@ public class ProgressActivity extends AppCompatActivity {
                 try {
 //                long before = System.currentTimeMillis();
                     for (int i = 1; i < 25; i++) {
-                        String lClassCd = i < 10 ? "0" + i : i + "";
-                        String queryUrl = "http://apis.data.go.kr/B490007/ncsStudyModule/openapi21?serviceKey=" + API_KEY
-                                + "&pageNo=" + "1" + "&numOfRows=" + "1600" + "&returnType=xml" + "&ncsLclasCd=" + lClassCd;
+                        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B490007/ncsStudyModule/openapi21"); /*URL*/
+                        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "="+API_KEY); /*Service Key*/
+                        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+                        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+                        urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8")); /*전송받을 페이지 타입*/
+                        urlBuilder.append("&" + URLEncoder.encode("ncsLclasCd","UTF-8") + "=" + URLEncoder.encode("01", "UTF-8")); /*대분류코드*/
+                        URL url = new URL(urlBuilder.toString());
+//                        String lClassCd = i < 10 ? "0" + i : i + "";
+//                        String queryUrl = "http://apis.data.go.kr/B490007/ncsStudyModule/openapi21?serviceKey=" + API_KEY
+//                                + "&pageNo=" + "1" + "&numOfRows=" + "16" + "&returnType=xml" + "&ncsLclasCd=" + lClassCd;
+//
+//                        URL url = new URL(queryUrl);
+                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                        con.setRequestMethod("GET");
+                        con.setDoOutput(true);
+                        con.setDoInput(true);
+                        con.setRequestProperty("Content-type", "application/xml");
+                        con.setRequestProperty("Accept", "application/xml");
 
-                        URL url = new URL(queryUrl);
-                        InputStream is = url.openStream();
+                        Log.e("progress",con.getResponseCode()+"");
+
+//                        InputStream is = con.getInputStream();
+                        if(con.getResponseCode() >= 200 && con.getResponseCode() <= 300) {
+                        } else {
+                            BufferedReader rd = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                            StringBuilder sb = new StringBuilder();
+                            String line;
+                            while ((line = rd.readLine()) != null) {
+                                sb.append(line);
+                            }
+                            rd.close();
+                            Log.e("progress",sb.toString());
+                        }
+
+                        InputStream is  = con.getInputStream();
 
                         // xml parsing
                         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
@@ -144,14 +177,13 @@ public class ProgressActivity extends AppCompatActivity {
                             }
                             eventType = xpp.next();
                         }
-
+                        con.disconnect();
                     }
                     startActivity(new Intent(this, MainActivity.class));
                     db.close();
                     finish();
 //                long after = System.currentTimeMillis();
 //                Log.d(TAG, (after - before) / 1000 + "");
-//                    db.close();
                 } catch (Exception e) {
                     Log.e(TAG, Log.getStackTraceString(e));
                 }
